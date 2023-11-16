2a4c7ac9b2bf8c7d3fbe24c56054d071d9a8048e
Clueed <clueed@proton.me>
2023-11-16 15:06:55 +0100

    backup.sh: better messages

--- backup.sh
+++ backup.sh
@@ -9,18 +9,32 @@ if [ ! -d "$backup_dir" ]; then
   echo "Created backup directory: $backup_dir"
 fi
 
+echo "Starting backup process..."
+
 cd "$backup_dir"
 
 timestamp=$(date +"%Y%m%d_%H%M%S")
+echo "Timestamp for backup: $timestamp"
+
+echo "Compressing worlds directory..."
 
 tar -zcvf "Worlds_$timestamp.tar.gz" -C "$worlds_directory" .
+echo "Backup file created: Worlds_$timestamp.tar.gz"
 
+echo "Uploading backup to S3..."
 aws s3 cp "Worlds_$timestamp.tar.gz" s3://${R2_BUCKET_NAME}/ --endpoint-url https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com
+echo "Backup uploaded to S3"
 
+echo "Cleaning up temporary backup file..."
 rm "Worlds_$timestamp.tar.gz"
+echo "Temporary backup file removed"
+
+echo "Checking existing backup files in S3..."
 
 file_list=$(aws s3 ls s3://calamine/ --endpoint-url https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com | awk '{print $4}' | sort)
 file_count=$(echo "$file_list" | wc -l)
+
+echo "Number of existing backup files: $file_count"
 
 files_to_delete=$((file_count - max_files))
 
@@ -31,5 +45,8 @@ if [ $files_to_delete -gt 0 ]; then
 
   for file_key in $files_to_delete_list; do
     aws s3 rm s3://calamine/"$file_key" --endpoint-url https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com
+    echo "Deleted old backup file: $file_key"
   done
 fi
+
+echo "Backup process completed."
